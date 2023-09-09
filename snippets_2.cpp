49adcda11144dcadb2baccaf4528ef13cc613987
Navneet Gupta <88278303+navneetguptacse@users.noreply.github.com>
2023-09-10 00:43:33 +0530

    Add files via upload

---
+++ snippets_2.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// macros..
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define f first
+#define s second
+#define vr(v) v.begin(),v.end()
+#define rv(v) v.end(),v.begin()
+
+// def..
+typedef long long ll;
+typedef vector<vector<long long>> vvll;
+typedef vector<long long> vll;
+
+void solve() {
+    /** write your code here..*/
+}
+
+int main() {
+    cout<<fixed<<setprecision(3);
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int t=1, i=1;
+    // cin >> t;
+    while(i<=t) {
+        // cout << "Case #" << i << ": ";
+        solve();
+        i++;
+    }
+    return 0;
+}
